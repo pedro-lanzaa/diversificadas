26c6a806eb9926a1fc963c863875016910053f1e
Pedro Lanza <pedrooliveiralanza@gmail.com>
2024-06-17 11:54:09 -0300

    Update tênis.c

--- maratona/tênis.c
+++ maratona/tênis.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+  char resultado[6];
+  int vitoria = 0;
+
+  // Resultado dos 6 jogos
+  printf("Digite os resultados dos jogos utilizando V ou P!\n");
+  
+  for (int y = 0; y < 6; y++) {
+
+    scanf(" %c", &resultado[y]);
+
+    if (resultado[y] == 'V') {
+      vitoria++;
+    }
+  }
+
+  // Determina o grupo
+  if (vitoria >= 5) {
+    printf("Parabéns, você está no grupo 1\n");
+
+  } else if (vitoria >=3) {
+    printf("Parabéns, você está no grupo 2\n");
+
+  } else if (vitoria >=1) {
+    printf("Parabéns, você está no grupo 3\n");
+
+  } else {
+    printf("Sinto muito, você não entrou para nenhum grupo -1\n");
+  }
+
+  return 0;
+}
